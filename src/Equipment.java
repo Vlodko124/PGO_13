1533f3d6d164c42e1451840ddddac4588df23319
Vlodko124 <s30198@pjwstk.edu.pl>
2026-06-08 23:13:38 +0200

    wdeferdgfvfdrewqdfd

--- src/Equipment.java
+++ src/Equipment.java
@@ -1,2 +1,54 @@
-public class Equipment {
+public abstract class Equipment implements Displayable {
+    private String id;
+    private String name;
+    private double baseDailyPrice;
+    private boolean available;
+
+    public Equipment(String id, String name, double baseDailyPrice) {
+        this.id = id;
+        this.name = name;
+        this.baseDailyPrice = baseDailyPrice;
+        this.available = true;
+    }
+
+    public abstract double calculateDailyPrice();
+
+    public abstract String getDetails();
+
+    public String getEquipmentType() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    public String getDisplayText() {
+        return String.format(
+                "%s | %s | Type: %s | Price: %.2f PLN/day | %s | %s",
+                id,
+                name,
+                getEquipmentType(),
+                calculateDailyPrice(),
+                available ? "Available" : "Not available",
+                getDetails()
+        );
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBaseDailyPrice() {
+        return baseDailyPrice;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
